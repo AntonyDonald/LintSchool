01473c95730260f763e15e28865353a2dbac1361
Antony Donald <antonydonald98@gmail.com>
2023-09-22 18:50:24 +0530

    Antony

--- src/screen/Dashboard/Likes.js
+++ src/screen/Dashboard/Likes.js
@@ -1,20 +1,50 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View, } from 'react-native'
 import React from 'react'
-import Root from '../../components/CustomComponent/Root'
+import RBSheet from 'react-native-raw-bottom-sheet'
+import { useTheme } from '@react-navigation/native'
+import { useRef } from 'react'
+import OptionBar from '../../components/BottomSheet/OpitonBar'
+import { Button } from 'react-native-paper'
 
 const Likes = () => {
+    const { colors } = useTheme();
+    const refRBSheet = useRef();
     return (
-        <Root>
-            <SafeAreaView style={{ flex: 1 }}>
-                <View style={{
-                    flex: 1,
-                    justifyContent: 'center',
-                    alignItems: 'center'
-                }}>
-                    <Text>Likes</Text>
-                </View>
-            </SafeAreaView>
-        </Root>
+        <>
+            <RBSheet
+                ref={refRBSheet}
+                closeOnDragDown={true}
+                height={270}
+                openDuration={100}
+                customStyles={{
+                    wrapper: {
+                    },
+                    container: {
+                        backgroundColor: colors.card,
+                        borderTopLeftRadius: 15,
+                        borderTopRightRadius: 15,
+                    },
+                    draggableIcon: {
+                        marginTop: 5,
+                        marginBottom: 0,
+                        height: 5,
+                        width: 90,
+                        backgroundColor: colors.borderColor,
+                    }
+                }}
+            >
+                <OptionBar />
+
+            </RBSheet>
+
+            <View style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}>
+                <Button onPress={() => refRBSheet.current.open()}>Open Bottom Sheet</Button>
+            </View>
+        </>
     )
 }
 
